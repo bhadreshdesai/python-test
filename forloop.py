1e7607aba642429b08c0ddf1029afae21921bddb
bhadreshdesai <bhadresh_desai@hotmail.com>
2021-01-08 14:42:12 +0000

    For loop test

---
+++ forloop.py
@@ -0,0 +1,7 @@
+print('For loop printing numbers 1 t0 10')
+for i in range(1, 11):
+    print(i)
+
+print('For loop printing number1 to 10 skipping every other number')
+for i in range(1, 11, 2):
+    print(i)
